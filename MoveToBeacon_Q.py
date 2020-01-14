e88db45f839e94f92e3efd36d682d081dc8d62ba
Pacmega <bas.witters@hotmail.com>
2020-01-14 12:32:49 +0100

    get_state fixed, hardcoded ver finished, Qtable ver base working

--- MoveToBeacon_Q.py
+++ MoveToBeacon_Q.py
@@ -20,9 +20,9 @@ epsilon = 0.9           # Chance of randomly selected action instead of followin
 eps_decay_rate = 0.005  # Decay rate of epsilon
 min_epsilon = 0.01      # Do not decay epsilon below this point
 
+# Q-learning settings
 qtable_size = 20 # Instead of 84x84 observation space, reduce to this dimension
 
-# State is a tuple: (selected_unit (ID), marine_loc_x, marine_loc_y, beacon_centre_loc_x, beacon_centre_loc_y)
 def get_state(observation):
 	"""
 	Returns a simplified version of the game state:
@@ -33,12 +33,21 @@ def get_state(observation):
 	beacon_centre_location_y - int between 0 and qtable_size
 	"""
 	marine_sel = 0 # False, except as int (to represent everything as int)
+
 	if FUNCTIONS.Attack_screen.id in observation.available_actions:
 		# _PLAYER_SELF has one of their own units selected
 		marine_sel = 1 # True, except as int (to represent everything as int)
 
 	player_relative = observation.feature_screen.player_relative
 	marine_location = np.mean(_xy_locs(player_relative == _PLAYER_SELF), axis=0).round()
+
+	if type(marine_location) is not np.ndarray:
+		# API messed up and didn't properly return the marine's location. 
+		# This only seems to happen when the marine reaches the target, but is not consistent?
+		#  This is fine for action selection since attack-move only requires a target, 
+		#   but will break reward calculation if not handled outside of this function.
+		marine_location = np.zeros(2)
+
 	m_loc_reduced = tuple(coordinate / qtable_size for coordinate in marine_location)
 	beacon_centre_location = np.mean(_xy_locs(player_relative == _PLAYER_NEUTRAL), axis=0).round()
 	b_cen_reduced = tuple(coordinate / qtable_size for coordinate in beacon_centre_location)
@@ -57,6 +66,24 @@ def _xy_locs(mask):
 	y, x = mask.nonzero()
 	return list(zip(x, y))
 
+class MoveToBeacon_Qtable(base_agent.BaseAgent):
+	# Variables used
+	op_every = 10 # Keep a slightly human-like amount of Actions Per Minute
+	no_op_counter = 0
+	army_selected = False
+	selected_action = None
+
+	# Store the previous location in case the API loses the marine's location, tends to happen from time to time. No idea why.
+	prev_marine_loc = np.zeros(2)
+
+	def step(self, obs):
+		super(MoveToBeacon_Qtable, self).step(obs)
+		print(get_state(obs.observation))
+		print("Not implemented")
+		print("reward = ", obs.reward)
+		print("Reminder to check if marine_location is NOT [0, 0], since that messes with reward calculation")
+		return NO_OP
+
 class MoveToBeacon_hardcoded(base_agent.BaseAgent):
 	# Variables used
 	op_every = 10 # Keep a slightly human-like amount of Actions Per Minute
@@ -66,7 +93,6 @@ class MoveToBeacon_hardcoded(base_agent.BaseAgent):
 
 	def step(self, obs):
 		super(MoveToBeacon_hardcoded, self).step(obs)
-		print(get_state(obs.observation))
 		if self.no_op_counter == self.op_every:
 			if FUNCTIONS.Attack_screen.id in obs.observation.available_actions:
 				# Army units selected
